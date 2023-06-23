5f748a28324f1683f929fdd0c7006d86b3a9c958
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-06-23 18:00:27 +0200

    Refactor Controller.java

--- api/src/main/java/it/unicam/cs/mp/progettoesame/api/Controller.java
+++ api/src/main/java/it/unicam/cs/mp/progettoesame/api/Controller.java
@@ -2,20 +2,43 @@ package it.unicam.cs.mp.progettoesame.api;
 
 import it.unicam.cs.mp.progettoesame.api.models.IShape;
 import it.unicam.cs.mp.progettoesame.api.models.Robot;
+import it.unicam.cs.mp.progettoesame.utilities.FollowMeParser;
+import it.unicam.cs.mp.progettoesame.utilities.FollowMeParserException;
+import it.unicam.cs.mp.progettoesame.utilities.FollowMeParserHandler;
 
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 public class Controller {
     private List<Robot> robots;
     private List<IShape> shapes;
+    private final FollowMeParser parser;
     public Controller(List<Robot> robots, List<IShape> shapes) {
         this.robots = robots;
         this.shapes = shapes;
+        final FollowMeParserHandler handler = new ParserHandler(this.robots, this.shapes);
+        this.parser = new FollowMeParser(handler);
     }
 
     public void loadRobots(List<Robot> robots) { this.robots = robots; }
     public void loadShapes(List<IShape> shapes) { this.shapes = shapes; }
+
+    /**
+     * Metodo che si occupa di leggere il programma dei robot e di passarlo al
+     * parser
+     * @param programFile file da cui leggere
+     * @return lista di stringhe contenente tutto il programma
+     * @throws IOException se c'è qualche errore nella lettura
+     * @throws FollowMeParserException se c'è qualche errore nel programma
+     */
+    public List<String> readInstructionList(File programFile) throws IOException, FollowMeParserException {
+        List<String> lines = Files.readAllLines(programFile.toPath());
+        this.parser.parseRobotProgram(programFile);
+        return lines;
+    }
 
     public void loadProgramToRobots(Program program) {
         this.robots.forEach(x -> x.loadProgramToExecute(program));
